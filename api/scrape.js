c7ec44b3b4fbb06dcc12003b7d3c20b091c857c5
pamuches <102895496+pamuches@users.noreply.github.com>
2026-06-17 14:50:09 -0600

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
           content: `Eres un experto comparador de precios en México. Encuentra productos populares con descuento >15% y compara precios entre tiendas.
 
 ESTRATEGIA:
-1. Busca 15-20 productos populares (electrónica, hogar, tecnología)
+1. Busca 10-15 productos populares (electrónica, hogar, tecnología)
 2. Para CADA producto, intenta encontrarlo en 2-3 tiendas diferentes
 3. Compara precios del MISMO producto entre tiendas
 
